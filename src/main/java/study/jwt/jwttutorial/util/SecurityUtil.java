41b1c99f092a8b1e8f24142cb1fdba7af7716e4c
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-22 00:55:58 +0900

    SecurityUtill 생성 (#5)

---
+++ src/main/java/study/jwt/jwttutorial/util/SecurityUtil.java
@@ -0,0 +1,38 @@
+package study.jwt.jwttutorial.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public class SecurityUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(SecurityUtil.class);
+
+    private SecurityUtil() {
+    }
+
+    //SecurityContext의 Authentication 객체를 이용해 username을 리턴해주는 메소드
+    public static Optional<String> getCurrentUsername() {
+        //SecurityContext에 Authentication 객체가 저장되는 시점 : JwtFilter의 doFilter 메소드에서 Request 들어올 때
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            logger.debug("Security Context에 인증 정보가 없습니다.");
+            return Optional.empty();
+        }
+
+        String username = null;
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+            username = springSecurityUser.getUsername();
+        } else if (authentication.getPrincipal() instanceof String) {
+            username = (String) authentication.getPrincipal();
+        }
+
+        return Optional.ofNullable(username);
+    }
+}
