46a2084a18ec7f37546883c413639b71ea759935
yujin113 <claire9585@sookmyung.ac.kr>
2022-03-21 17:09:06 +0900

    Dto 생성 (#3)

---
+++ src/main/java/study/jwt/jwttutorial/dto/TokenDto.java
@@ -0,0 +1,13 @@
+package study.jwt.jwttutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
